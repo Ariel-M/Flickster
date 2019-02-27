e08db58913669695dd798eb59a4ad0503086282d
Ariel Moskowitz <arielsm86@gmail.com>
2019-02-26 22:11:46 -0500

    initial commit

---
+++ app/src/main/java/com/codepath/flickster/model/Movie.java
@@ -0,0 +1,47 @@
+package com.codepath.flickster.model;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+    String backdrop_path;
+    //constructor for adding movies
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        backdrop_path = jsonObject.getString("backdrop_path");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException{
+        List<Movie> movies = new ArrayList<>();
+        //uses the constructer to add movies from the JSONObject to the array
+        for(int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+    public String getBackdrop_path(){
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdrop_path);
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
